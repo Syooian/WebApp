95b3e0b2ac54007962ed4a79c95610c9ad154d23
Syooian <msn0126@hotmail.com>
2025-05-15 17:53:51 +0800

    Debug

--- Test1/Controllers/FileUploadController.cs
+++ Test1/Controllers/FileUploadController.cs
@@ -29,38 +29,47 @@ namespace Test1.Controllers
         [HttpPost]
         public IActionResult UploadFile(IFormFile Photo)
         {
-            if (Photo == null || Photo.Length == 0)
+            try
             {
-                ViewData["ErrorMsg"] = "無檔案";
+                if (Photo == null || Photo.Length == 0)
+                {
+                    ViewData["ErrorMsg"] = "無檔案";
 
-                return View();
+                    return View();
+                }
+                else if (Photo.ContentType != "image/jpeg" && Photo.ContentType != "image/png")//驗證MIME Type
+                {
+                    ViewData["ErrorMsg"] = "僅限上傳圖片";
+
+                    return View();
+                }
+
+                Debug.WriteLine("Uploading");
+
+                //取檔案名稱
+                var FileName = Path.GetFileName(Photo.FileName);
+                Debug.WriteLine("FileName : " + FileName);
+
+                //檔案上傳後要儲存的完整路徑
+                var FilePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "Photos", FileName);
+                Debug.WriteLine("FilePath : " + FilePath);
+
+                //檢查資料夾路徑
+                var DirectoryPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "Photos");
+                if (!Directory.Exists(DirectoryPath))
+                {
+                    Directory.CreateDirectory(DirectoryPath);
+                }
+
+                //將檔案上傳至指定路徑
+                using (FileStream FS = new FileStream(FilePath, FileMode.Create))
+                {
+                    Photo.CopyTo(FS);
+                }
             }
-            else if (Photo.ContentType != "image/jpeg" && Photo.ContentType != "image/png")//驗證MIME Type
+            catch(Exception ex)
             {
-                ViewData["ErrorMsg"] = "僅限上傳圖片";
-
-                return View();
-            }
-
-            //取檔案名稱
-            var FileName = Path.GetFileName(Photo.FileName);
-            Debug.WriteLine("FileName : " + FileName);
-
-            //檔案上傳後要儲存的完整路徑
-            var FilePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "Photos", FileName);
-            Debug.WriteLine("FilePath : " + FilePath);
-
-            //檢查資料夾路徑
-            var DirectoryPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "Photos");
-            if (!Directory.Exists(DirectoryPath))
-            {
-                Directory.CreateDirectory(DirectoryPath);
-            }
-
-            //將檔案上傳至指定路徑
-            using (FileStream FS = new FileStream(FilePath, FileMode.Create))
-            {
-                Photo.CopyTo(FS);
+                Debug.WriteLine("上傳失敗 : " + ex.Message);
             }
 
 
@@ -77,7 +86,10 @@ namespace Test1.Controllers
 
             foreach (var File in Files)
             {
+                Debug.WriteLine("F1 : " + File);
+
                 var FileName = Path.GetFileName(File);
+                Debug.WriteLine("F2 : " + FileName);
                 ViewData["Photos"] += $"<img src='/Photos/{FileName}' style='width:200px;height:200px;'><br>";
             }
 
