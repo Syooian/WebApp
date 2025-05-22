2e911a52fe9b78122af079a147d0e0a6e30f25f4
Syooian <msn0126@hotmail.com>
2025-05-22 10:49:29 +0800

    模擬帳號登入後至頁面

---
+++ My/Test2/Controllers/LoginController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Test2.Models;
+
+namespace Test2.Controllers
+{
+    public class LoginController : Controller
+    {
+        /// <summary>
+        /// 登入頁面
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Login()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 登入表單回傳
+        /// </summary>
+        /// <param name="LoginData"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public IActionResult Login(Login LoginData)
+        {
+            //帳密檢查
+            if (LoginData.UserName != "admin" || LoginData.Password != "123456")
+            {
+                ViewData["LoginError"] = "帳號或密碼錯誤";
+
+                return View();
+            }
+
+            //重新導向至HomeController的Index
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ My/Test2/Models/Login.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Cors;
+using System.ComponentModel.DataAnnotations;
+
+namespace Test2.Models
+{
+    public class Login
+    {
+        /// <summary>
+        /// 使用者名稱
+        /// </summary>
+        [Display(Name = "帳號")]
+        [Required(ErrorMessage = "請輸入帳號")]
+        public string UserName { get; set; } = null!;
+        /// <summary>
+        /// 密碼
+        /// </summary>
+        [Display(Name = "密碼")]
+        [Required(ErrorMessage = "請輸入密碼")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = null!;
+        /// <summary>
+        /// 
+        /// </summary>
+        public Login()
+        {
+
+        }
+    }
+}
