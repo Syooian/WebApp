63fa293dfc5ef64904c4770596e8ba22b5877490
Syooian <msn0126@hotmail.com>
2025-05-22 09:45:33 +0800

    前台模擬

--- My/Test2/Controllers/HomeController.cs
+++ My/Test2/Controllers/HomeController.cs
@@ -11,24 +11,42 @@ namespace Test2.Controllers
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
-        }
 
-        public IActionResult Index()
-        {
+            Debug.WriteLine("HomeController");
+
             string[] id = { "A01", "A02", "A03", "A04", "A05", "A06", "A07" };
             string[] name = { "瑞豐夜市", "新堀江商圈", "六合夜市", "青年夜市", "花園夜市", "大東夜市", "武聖夜市" };
             string[] address = { "813高雄市左營區裕誠路", "800高雄市新興區玉衡里", "800台灣高雄市新興區六合二路",
                 "80652高雄市前鎮區凱旋四路758號", "台南市北區海安路三段533號", "台南市東區林森路一段276號",
                 "台南市中西區武聖路69巷42號" };
 
-            List<NightMarket> Data = new List<NightMarket>();
+            NightMarketData = new List<NightMarket>();
 
             for (int a = 0; a < id.Length; a++)
             {
-                Data.Add(new NightMarket(id[a], name[a], address[a]));
+                NightMarketData.Add(new NightMarket(id[a], name[a], address[a]));
             }
+        }
 
-            return View(Data);
+        /// <summary>
+        /// Guest
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult IndexRWD()
+        {
+            return View(NightMarketData);
+        }
+
+        /// <summary>
+        /// 夜市主資料
+        /// </summary>
+        List<NightMarket> NightMarketData;
+
+        public IActionResult Index()
+        {
+            Debug.WriteLine("Index");
+
+            return View(NightMarketData);
         }
 
         /// <summary>
