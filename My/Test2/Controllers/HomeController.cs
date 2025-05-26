e03470e5294d6b35b3be984b4bde8eeb1121e3f1
Syooian <msn0126@hotmail.com>
2025-05-26 09:29:15 +0800

    NightMarketData摘出 詳細資料頁面 Linq語法找資料

--- My/Test2/Controllers/HomeController.cs
+++ My/Test2/Controllers/HomeController.cs
@@ -14,19 +14,6 @@ namespace Test2.Controllers
 
             Debug.WriteLine("HomeController");
 
-            string[] id = { "A01", "A02", "A03", "A04", "A05", "A06", "A07" };
-            string[] name = { "瑞豐夜市", "新堀江商圈", "六合夜市", "青年夜市", "花園夜市", "大東夜市", "武聖夜市" };
-            string[] address = { "813高雄市左營區裕誠路", "800高雄市新興區玉衡里", "800台灣高雄市新興區六合二路",
-                "80652高雄市前鎮區凱旋四路758號", "台南市北區海安路三段533號", "台南市東區林森路一段276號",
-                "台南市中西區武聖路69巷42號" };
-
-            NightMarketData = new List<NightMarket>();
-
-            for (int a = 0; a < id.Length; a++)
-            {
-                NightMarketData.Add(new NightMarket(id[a], name[a], address[a]));
-            }
-
             /*
              * _Layout.cshtml
              * 所有view共享同一Layout，且不可單獨存在，需依附別的script
@@ -42,19 +29,35 @@ namespace Test2.Controllers
         /// <returns></returns>
         public IActionResult IndexRWD()
         {
-            return View(NightMarketData);
+            return View(NightMarketData());
         }
 
         /// <summary>
         /// 夜市主資料
         /// </summary>
-        List<NightMarket> NightMarketData;
+        List<NightMarket> NightMarketData()
+        {
+            string[] id = { "A01", "A02", "A03", "A04", "A05", "A06", "A07" };
+            string[] name = { "瑞豐夜市", "新堀江商圈", "六合夜市", "青年夜市", "花園夜市", "大東夜市", "武聖夜市" };
+            string[] address = { "813高雄市左營區裕誠路", "800高雄市新興區玉衡里", "800台灣高雄市新興區六合二路",
+                "80652高雄市前鎮區凱旋四路758號", "台南市北區海安路三段533號", "台南市東區林森路一段276號",
+                "台南市中西區武聖路69巷42號" };
+
+            var NightMarketData = new List<NightMarket>();
+
+            for (int a = 0; a < id.Length; a++)
+            {
+                NightMarketData.Add(new NightMarket(id[a], name[a], address[a]));
+            }
+
+            return NightMarketData;
+        }
 
         public IActionResult Index()
         {
             Debug.WriteLine("Index");
 
-            return View(NightMarketData);
+            return View(NightMarketData());
         }
 
         /// <summary>
@@ -80,6 +83,37 @@ namespace Test2.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="ID"></param>
+        /// <returns></returns>
+        public IActionResult Details(string ID)
+        {
+            // Home/Details?ID=A01
+
+            var List = NightMarketData();
+
+            //1
+            var Result = (from N in List where N.ID == ID select N).FirstOrDefault();
+
+            //2
+            //NightMarket Result = null;
+            //for (int a = 0; a < List.Count; a++)
+            //{
+            //    if (List[a].ID == ID)
+            //    {
+            //        Result = List[a];
+            //        break;
+            //    }
+            //}
+
+            //3
+            //var Result = List.Find(N => N.ID == ID);
+
+            return View(Result);
         }
     }
 }
