ba11c4c42f58cb275992739bf805ce4144ca14ff
Syooian <msn0126@hotmail.com>
2025-06-09 23:57:02 +0800

    安裝套件：Microsoft.VisualStudio.Web.CodeGeneration.Design v8.0.7 完成剩餘步驟

--- My/DBFirst_Test/Models/dbstudentsContext.cs
+++ My/DBFirst_Test/Models/dbstudentsContext.cs
@@ -6,10 +6,18 @@ namespace DBFirst_Test.Models;
 
 public partial class dbstudentsContext : DbContext
 {
-    public dbstudentsContext(DbContextOptions<dbstudentsContext> options)
-        : base(options)
+    //public dbstudentsContext(DbContextOptions<dbstudentsContext> options)
+    //    : base(options)
+    //{
+    //}
+
+    public dbstudentsContext()
     {
     }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseMySql("Server=127.0.0.1;Database=dbstudents;User=root;Password=autc007;",
+            new MySqlServerVersion(new Version(8, 0, 31)));
 
     public virtual DbSet<tstudent> tstudent { get; set; }
 
@@ -26,12 +34,8 @@ public partial class dbstudentsContext : DbContext
             entity.Property(e => e.fStuId)
                 .HasMaxLength(6)
                 .IsFixedLength();
-            entity.Property(e => e.fEmail)
-                .HasMaxLength(40)
-                .HasDefaultValueSql("''");
-            entity.Property(e => e.fName)
-                .HasMaxLength(30)
-                .HasDefaultValueSql("''");
+            entity.Property(e => e.fEmail).HasMaxLength(40);
+            entity.Property(e => e.fName).HasMaxLength(30);
             entity.Property(e => e.fScore)
                 .HasDefaultValueSql("'0'")
                 .HasColumnType("int(11)");
