d3f6728b0cd7eed7bc24e01b299862866f1f059c
Syooian <msn0126@hotmail.com>
2025-06-09 22:49:13 +0800

    新增README.md 自動生成Model

---
+++ My/DBFirst_Test/Models/dbstudentsContext.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace DBFirst_Test.Models;
+
+public partial class dbstudentsContext : DbContext
+{
+    public dbstudentsContext(DbContextOptions<dbstudentsContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<tstudent> tstudent { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder
+            .UseCollation("utf8mb4_uca1400_ai_ci")
+            .HasCharSet("utf8mb4");
+
+        modelBuilder.Entity<tstudent>(entity =>
+        {
+            entity.HasKey(e => e.fStuId).HasName("PRIMARY");
+
+            entity.Property(e => e.fStuId)
+                .HasMaxLength(6)
+                .IsFixedLength();
+            entity.Property(e => e.fEmail)
+                .HasMaxLength(40)
+                .HasDefaultValueSql("''");
+            entity.Property(e => e.fName)
+                .HasMaxLength(30)
+                .HasDefaultValueSql("''");
+            entity.Property(e => e.fScore)
+                .HasDefaultValueSql("'0'")
+                .HasColumnType("int(11)");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ My/DBFirst_Test/README.md
@@ -0,0 +1,4 @@
+選擇DBFirst_Test為啟動專案
+套件管理主控台的預設專案選擇DBFirst_Test
+Scaffold-DbContext生成Model操作：\
+Scaffold-DbContext "Server='127.0.0.1';Database='dbstudents';User='root';Password='autc007'" Pomelo.EntityFrameworkCore.MySql -OutputDir Models -NoOnConfiguring -UseDatabaseNames -NoPluralize -Force
