11dff6148cab38763ba591d8c18d10882c5b6987
Syooian <msn0126@hotmail.com>
2025-06-12 09:05:49 +0800

    3.2 撰寫在View上顯示的欄位內容(Display)

--- My/DBFirst/Models/tStudent.cs
+++ My/DBFirst/Models/tStudent.cs
@@ -1,15 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace DBFirst.Models;
 
 public partial class tStudent
 {
+    /// <summary>
+    /// 學號
+    /// </summary>
+    [Display(Name = "學號")]
     public string fStuId { get; set; } = null!;
-
+    /// <summary>
+    /// 姓名
+    /// </summary>
+    [Display(Name = "姓名")]
     public string fName { get; set; } = null!;
-
+    /// <summary>
+    /// 電子郵件
+    /// </summary>
+    [Display(Name = "電子郵件")]
     public string? fEmail { get; set; }
-
+    /// <summary>
+    /// 成績
+    /// </summary>
+    [Display(Name = "成績")]
     public int? fScore { get; set; }
 }
--- Teacher/MyModel_DBFirst/Models/tStudent.cs
+++ Teacher/MyModel_DBFirst/Models/tStudent.cs
@@ -1,15 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace MyModel_DBFirst.Models;
 
 public partial class tStudent
 {
+    //3.2 撰寫在View上顯示的欄位內容(Display)
+
+    [Display(Name ="學號")]
     public string fStuId { get; set; } = null!;
 
+    [Display(Name = "姓名")]
     public string fName { get; set; } = null!;
 
+    [Display(Name = "電子郵件")]
     public string? fEmail { get; set; }
 
+    [Display(Name = "成績")]
     public int? fScore { get; set; }
 }
