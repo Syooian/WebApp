d0daeda4af9198b394572f53fea2259acabcdd03
Syooian <msn0126@hotmail.com>
2025-06-19 09:07:33 +0800

    5.8.1~5.8.3 建立ViewModel

---
+++ Teacher/MyModel_DBFirst/ViewModels/VMtStudent.cs
@@ -0,0 +1,10 @@
+﻿using MyModel_DBFirst.Models;
+
+namespace MyModel_DBFirst.ViewModels
+{
+    public class VMtStudent
+    {
+        public List<tStudent> Students { get; set; } //tStudent資料表的資料
+        public List<Department> Departments { get; set; }
+    }
+}
