87bd9dfaa1fb9aeda461922afc34fe6160c3d0e8
Syooian <msn0126@hotmail.com>
2025-06-23 21:09:02 +0800

    生成DBContext和相關Model

---
+++ My/DBFirst_MySql2/Models/dbstudentsContext.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace DBFirst_MySql2.Models;
+
+public partial class dbstudentsContext : DbContext
+{
+    public dbstudentsContext(DbContextOptions<dbstudentsContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<department> department { get; set; }
+
+    public virtual DbSet<tstudent> tstudent { get; set; }
+
+    public virtual DbSet<tstudent2> tstudent2 { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder
+            .UseCollation("utf8mb4_uca1400_ai_ci")
+            .HasCharSet("utf8mb4");
+
+        modelBuilder.Entity<department>(entity =>
+        {
+            entity.HasKey(e => e.DeptID).HasName("PRIMARY");
+
+            entity.ToTable(tb => tb.HasComment("科系"));
+
+            entity.Property(e => e.DeptID)
+                .HasMaxLength(2)
+                .HasComment("科系ID");
+            entity.Property(e => e.DeptName)
+                .HasMaxLength(50)
+                .HasComment("科系名稱");
+        });
+
+        modelBuilder.Entity<tstudent>(entity =>
+        {
+            entity.HasKey(e => e.fStuId).HasName("PRIMARY");
+
+            entity.Property(e => e.fStuId)
+                .HasMaxLength(6)
+                .IsFixedLength();
+            entity.Property(e => e.fEmail).HasMaxLength(40);
+            entity.Property(e => e.fName).HasMaxLength(30);
+            entity.Property(e => e.fScore)
+                .HasDefaultValueSql("'0'")
+                .HasColumnType("int(11)");
+        });
+
+        modelBuilder.Entity<tstudent2>(entity =>
+        {
+            entity.HasKey(e => e.fStuId).HasName("PRIMARY");
+
+            entity.HasIndex(e => e.DeptID, "FK_tstudent2_department");
+
+            entity.Property(e => e.fStuId)
+                .HasMaxLength(6)
+                .IsFixedLength()
+                .HasComment("學號");
+            entity.Property(e => e.DeptID)
+                .HasMaxLength(2)
+                .HasComment("科系ID");
+            entity.Property(e => e.fEmail)
+                .HasMaxLength(40)
+                .HasComment("電子郵件");
+            entity.Property(e => e.fName)
+                .HasMaxLength(30)
+                .HasComment("姓名");
+            entity.Property(e => e.fScore)
+                .HasDefaultValueSql("'0'")
+                .HasComment("分數")
+                .HasColumnType("int(11)");
+
+            entity.HasOne(d => d.Dept).WithMany(p => p.tstudent2)
+                .HasForeignKey(d => d.DeptID)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_tstudent2_department");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ My/DBFirst_MySql2/Models/tstudent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DBFirst_MySql2.Models;
+
+public partial class tstudent
+{
+    public string fStuId { get; set; } = null!;
+
+    public string fName { get; set; } = null!;
+
+    public string fEmail { get; set; } = null!;
+
+    public int? fScore { get; set; }
+}
