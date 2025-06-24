e486a9d46a285a466e1eb1c4c40b3dd687c046ae
Syooian <msn0126@hotmail.com>
2025-06-24 10:31:52 +0800

    建立ViewModel

---
+++ My/DBFirst_MySql2/ViewModels/VM_tStudent2_Department.cs
@@ -0,0 +1,16 @@
+﻿using DBFirst_MySql2.Models;
+
+namespace DBFirst_MySql2.ViewModels
+{
+    public class VM_tStudent2_Department
+    {
+        /// <summary>
+        /// 科系
+        /// </summary>
+        public List<department>? Departments { get; set; }
+        /// <summary>
+        /// 學生
+        /// </summary>
+        public List<tstudent2>? Students { get; set; }
+    }
+}
