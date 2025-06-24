b4581dbb302a5f9119728627d25af31e34e3faea
Syooian <msn0126@hotmail.com>
2025-06-24 11:44:25 +0800

    增加時間顯示

---
+++ My/DBFirst_MySql2/wwwroot/js/UpdateTime.js
@@ -0,0 +1,14 @@
+﻿function UpdateTime(ObjID) {
+    //回傳時間格式： yyyy/MM/dd HH:mm:ss
+
+    const Now = new Date();
+    const FormattedStr =
+        Now.getFullYear() + "/" +
+        String(Now.getMonth() + 1).padStart(2, '0') + "/" +
+        String(Now.getDate()).padStart(2, '0') + " " +
+        String(Now.getHours()).padStart(2, '0') + ":" +
+        String(Now.getMinutes()).padStart(2, '0') + ":" +
+        String(Now.getSeconds()).padStart(2, '0');
+
+    document.getElementById(ObjID).innerHTML = FormattedStr;
+}
