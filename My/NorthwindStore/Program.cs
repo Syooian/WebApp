70bb52212ce1596762cf527c84306e15ff6076c1
Syooian <msn0126@hotmail.com>
2025-06-25 09:24:31 +0800

    資料庫連線

--- My/NorthwindStore/Program.cs
+++ My/NorthwindStore/Program.cs
@@ -1,7 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<NorthwindStore.Models.NorthwindContext>(Options =>
+    Options.UseSqlServer(builder.Configuration.GetConnectionString("DBConnectionStrings")));
 
 var app = builder.Build();
 
