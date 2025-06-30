b02576998fcf6b8b512551558faafcc7e6b8a53b
Syooian <msn0126@hotmail.com>
2025-06-30 11:09:25 +0800

    1.2.1 建立自訂的DBConext

---
+++ Teacher/MyModel_CodeFirst/Models/GuestBookContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MyModel_CodeFirst.Models
+{
+    public class GuestBookContext : DbContext
+    {
+        //慣例上會使用資料庫的名稱做檔名
+        //e.g. 資料庫叫做GuestBook，檔名就會取GuestBookContext.cs
+
+
+    }
+}
