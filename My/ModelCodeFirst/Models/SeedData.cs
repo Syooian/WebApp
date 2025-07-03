2a815af17b0542a2c58d2ade17f35bb4657f71ad
Syooian <msn0126@hotmail.com>
2025-07-03 11:10:02 +0800

    1.3.2~1.3.3-2 建立SeedData

---
+++ My/ModelCodeFirst/Models/SeedData.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ModelCodeFirst.Models
+{
+    public class SeedData
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="ServiceProvider"></param>
+        public static void Initialize(IServiceProvider ServiceProvider)
+        {
+            using (var context = new GuestBookContext(
+                ServiceProvider.GetRequiredService<DbContextOptions<GuestBookContext>>()))
+            {
+                // Look for any books.
+                if (context.Book.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                //var BookData = new Book[5];
+                //for (int a = 0; a < BookData.Length; a++)
+                //{
+                //    var NewGUID = Guid.NewGuid().ToString();
+
+                //    BookData[a] = new Book
+                //    {
+                //        ID = NewGUID,
+                //        Title = $"Book {a + 1}",
+                //        Description = $"This is book number {a + 1}.",
+                //        Author = $"Author {a + 1}",
+                //        CreatedDate = DateTime.Now.AddMinutes(10),
+                //        Photo = $"{NewGUID}{(a + 1).ToString("0000")}.jpg"
+                //    };
+                //}
+
+                //context.Book.AddRange(BookData);
+                //==============================================
+                for (int a = 0; a < 5; a++)
+                {
+                    var NewGUID = Guid.NewGuid().ToString();
+
+                    var BookData = new Book
+                    {
+                        ID = NewGUID,
+                        Title = $"Book {a + 1}",
+                        Description = $"This is book number {a + 1}.",
+                        Author = $"Author {a + 1}",
+                        CreatedDate = DateTime.Now.AddMinutes(10),
+                        Photo = $"{NewGUID}{(a + 1).ToString("0000")}.jpg"
+                    };
+
+                    context.Book.Add(BookData);
+
+                    #region 留言回覆
+                    for (int b = 0; b < 3; b++)
+                    {
+                        var ReBookData = new ReBook
+                        {
+                            ID = BookData.ID,
+                            ReID = Guid.NewGuid().ToString(), //關聯到留言的ID
+                            Author = $"ReAuthor {a + 1}-{b + 1}",
+                            CreatedDate = DateTime.Now.AddMinutes(10),
+                            Description = $"This is reply number {b + 1} for book {a + 1}."
+                        };
+
+                        context.ReBook.Add(ReBookData);
+                    }
+                    #endregion
+                }
+                context.SaveChanges();
+            }
+        }
+    }
+}
