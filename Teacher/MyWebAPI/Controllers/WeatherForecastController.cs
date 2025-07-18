b14349b7b44b4f040101e3eb886af7e6659d62db
Syooian <msn0126@hotmail.com>
2025-07-18 11:43:32 +0800

    2.1.2 將ProductPhotos資料夾及內含之檔案放至專案中的wwwroot下

--- Teacher/MyWebAPI/Controllers/WeatherForecastController.cs
+++ Teacher/MyWebAPI/Controllers/WeatherForecastController.cs
@@ -76,3 +76,15 @@ namespace MyWebAPI.Controllers
 //2.1.2 將ProductPhotos資料夾及內含之檔案放至專案中的wwwroot下
 //2.1.3 在Program.cs中加入app.UseStaticFiles(); (因為我們開的是 純WebAPI專案)
 //2.1.4 在瀏覽器中輸入「http://localhost:XXXX/ProductPhotos/A3001.jpg」(XXXX為您的port)測試是否能看到照片
+
+
+//2.2   建立專案與資料庫的連線
+//2.2.1 使用DB First建立 Model
+//2.2.2 使用NuGet(專案名稱上按右鍵→管理NuGet套件)安裝下列套件
+//      (1) Microsoft.EntityFrameworkCore.SqlServer
+//      (2) Microsoft.EntityFrameworkCore.Tools 
+//2.2.3 到套件管理器主控台(檢視 > 其他視窗 > 套件管理器主控台)下指令
+//      Scaffold-DbContext "Data Source=伺服器位址;Database=GoodStore;TrustServerCertificate=True;User ID=帳號;Password=密碼" Microsoft.EntityFrameworkCore.SqlServer -OutputDir Models -NoOnConfiguring  -UseDatabaseNames -NoPluralize -Force
+//      若成功的話，會看到Build succeeded.字眼，並在Models資料夾裡看到GoodStoreContext.cs、Category.cs、Product.cs等資料庫相關類別檔
+//2.2.4 在appsettings.json檔中撰寫連線字串(ConnectionString)
+//2.2.5 在Program.cs註冊DbContext物件(GoodStoreContext.cs)並指定appsettings.json中的連線字串程式碼(這段必須寫在var builder這行後面)
