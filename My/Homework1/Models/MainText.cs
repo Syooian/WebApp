072524b2d15895e41b0efc632aaa8eef5c8fbddd
Syooian <msn0126@hotmail.com>
2025-07-21 16:20:57 +0800

    建立主文模型 (未完)

---
+++ My/Homework1/Models/MainText.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Homework1.Models
+{
+    public class MainText
+    {
+        /// <summary>
+        /// 主文編號
+        /// </summary>
+        [Key]
+        [HiddenInput]
+        [StringLength(36, MinimumLength = 36)]
+        [Column(TypeName = "char")]
+        public string MainTextID { get; set; }
+        /// <summary>
+        /// 主題
+        /// </summary>
+        [Display(Name = "主題")]
+        [Required(ErrorMessage = "請輸入主題")]
+        [StringLength(20)]
+        public string Title { get; set; }
+        /// <summary>
+        /// 發表內容
+        /// </summary>
+        [Display(Name = "發表內容")]
+        [Required(ErrorMessage = "請輸入發表內容")]
+        [StringLength(100)]
+        public string Context { get; set; }
+        照片、照片類型、發表人、張貼時間)
+    }
+}
