d06695a2a218e686497f9435f31cd402c65eea9a
Syooian <msn0126@hotmail.com>
2025-07-22 09:00:53 +0800

    2.2   建立專案與資料庫的連線

---
+++ My/WebAPITest/Models/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebAPITest.Models;
+
+public partial class Product
+{
+    public string ProductID { get; set; } = null!;
+
+    public string ProductName { get; set; } = null!;
+
+    public decimal Price { get; set; }
+
+    public string? Description { get; set; }
+
+    public string Picture { get; set; } = null!;
+
+    public DateTime CreatedDate { get; set; }
+
+    public string CateID { get; set; } = null!;
+
+    public virtual Category Cate { get; set; } = null!;
+
+    public virtual ICollection<OrderDetail> OrderDetail { get; set; } = new List<OrderDetail>();
+}
--- My/WebAPITest/Program.cs
+++ My/WebAPITest/Program.cs
@@ -1,4 +1,7 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using Microsoft.EntityFrameworkCore;
+using WebAPITest.Data;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
@@ -6,6 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<GoodStoreContext>(Options =>
+    Options.UseSqlServer(builder.Configuration.GetConnectionString("DBConnectionStrings")));
 
 var app = builder.Build();
 
