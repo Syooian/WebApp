eed0f985328cbbd9d03f530d17883771b5447433
Syooian <msn0126@hotmail.com>
2025-07-24 00:08:30 +0800

    新增"新增文章"功能

--- My/Homework1/Controllers/HomeController.cs
+++ My/Homework1/Controllers/HomeController.cs
@@ -17,11 +17,46 @@ namespace Homework1.Controllers
             this.Context = Context;
         }
 
+        /// <summary>
+        /// 主文清單
+        /// </summary>
+        /// <returns></returns>
         public async Task<IActionResult> Index()
         {
             var Result = await Context.MainTexts.Include(R => R.Replies).OrderByDescending(T => T.CreatedDate).ToListAsync();
 
             return View(Result);
+        }
+
+        /// <summary>
+        /// 新增主文
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Create()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="MainText"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("MainTextID,Title,Content,UserName")] MainText MainText)
+        {
+            if (ModelState.IsValid)
+            {
+                MainText.CreatedDate = DateTime.Now;
+
+                Context.Add(MainText);
+                await Context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            Console.WriteLine("ModelState is invalid. Errors: " + string.Join(", ", ModelState.Values.SelectMany(v => v.Errors).Select(e => e.ErrorMessage)));
+
+            return View(MainText);
         }
 
         public IActionResult Privacy()
