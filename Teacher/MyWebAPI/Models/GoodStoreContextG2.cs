7a4f66a676c68a59e7b87cff041f2009bd05539d
Syooian <msn0126@hotmail.com>
2025-07-23 15:55:03 +0800

    4.7.5 加入base.OnModelCreating(modelBuilder);來繼承父類別所的方法

--- Teacher/MyWebAPI/Models/GoodStoreContextG2.cs
+++ Teacher/MyWebAPI/Models/GoodStoreContextG2.cs
@@ -7,6 +7,11 @@ namespace MyWebAPI.Models;
 
 public class GoodStoreContextG2 : GoodStoreContext
 {
+    public GoodStoreContextG2(DbContextOptions<GoodStoreContext> options)
+       : base(options)
+    {
+    }
+
 
     //4.6.5 修改GoodStoreContext，增加ProductDTO的DbSet屬性
     public virtual DbSet<ProductDTO> ProductDTO { get; set; }
@@ -17,6 +22,10 @@ public class GoodStoreContextG2 : GoodStoreContext
     {
         //4.6.8 修改GoodStoreContext的OnModelCreating()，標示ProductDTO為HasNoKey
         modelBuilder.Entity<ProductDTO>(entity=>entity.HasNoKey());
+
+
+        //4.7.5 加入base.OnModelCreating(modelBuilder);來繼承父類別所的方法
+        base.OnModelCreating(modelBuilder);
     }
 
 
