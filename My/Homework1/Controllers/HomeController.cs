603c40b52b7b04e7e858a69255bafaa739367767
Syooian <msn0126@hotmail.com>
2025-07-24 10:22:47 +0800

    新增文章時可上傳圖片

--- My/Homework1/Controllers/HomeController.cs
+++ My/Homework1/Controllers/HomeController.cs
@@ -40,21 +40,39 @@ namespace Homework1.Controllers
         /// 
         /// </summary>
         /// <param name="MainText"></param>
+        /// <param name="MainTextPhoto">圖片上傳</param>
         /// <returns></returns>
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("MainTextID,Title,Content,UserName")] MainText MainText)
+        public async Task<IActionResult> Create([Bind("MainTextID,Title,Content,UserName")] MainText MainText, IFormFile? MainTextPhoto)
         {
             if (ModelState.IsValid)
             {
                 MainText.CreatedDate = DateTime.Now;
+
+                //圖片上傳
+                if (MainTextPhoto != null && MainTextPhoto.Length != 0)
+                {
+                    //取新檔名
+                    var FileName = MainText.MainTextID + Path.GetExtension(MainTextPhoto.FileName);
+
+                    var FilePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "MainTextPhotos", FileName);
+
+                    using (FileStream FS = new FileStream(FilePath, FileMode.Create))
+                    {
+                        MainTextPhoto.CopyTo(FS);
+                    }
+
+                    MainText.Photo = MainText.MainTextID;
+                    MainText.PhotoType = Path.GetExtension(MainTextPhoto.FileName);
+                }
 
                 Context.Add(MainText);
                 await Context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
 
-            Console.WriteLine("ModelState is invalid. Errors: " + string.Join(", ", ModelState.Values.SelectMany(v => v.Errors).Select(e => e.ErrorMessage)));
+            //Console.WriteLine("ModelState is invalid. Errors: " + string.Join(", ", ModelState.Values.SelectMany(v => v.Errors).Select(e => e.ErrorMessage)));
 
             return View(MainText);
         }
