46897169364969e7e764f32a5ab670a5c1f70473
Syooian <msn0126@hotmail.com>
2025-07-24 11:43:24 +0800

    新增回覆留言、回覆彈出框

---
+++ My/Homework1/Controllers/RepliesController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Homework1.Data;
+using Homework1.Models;
+
+namespace Homework1.Controllers
+{
+    public class RepliesController : Controller
+    {
+        private readonly DBContext _context;
+
+        public RepliesController(DBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Replies/Create
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="MainTextID"></param>
+        /// <returns></returns>
+        public IActionResult Create(string MainTextID)
+        {
+            ViewData["MainTextID"] = MainTextID;
+            return View();
+        }
+
+        // POST: Replies/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ReplyID,Content,CreatedDate,UserName,MainTextID")] Reply reply)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(reply);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["MainTextID"] = new SelectList(_context.MainTexts, "MainTextID", "MainTextID", reply.MainTextID);
+            return View(reply);
+        }
+
+        private bool ReplyExists(string id)
+        {
+            return _context.Replies.Any(e => e.ReplyID == id);
+        }
+    }
+}
