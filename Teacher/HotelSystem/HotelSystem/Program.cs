cbbb72639f08dd00be1840c5bd8f69079e7f229f
Syooian <msn0126@hotmail.com>
2025-08-04 15:33:49 +0800

    路由註冊

--- Teacher/HotelSystem/HotelSystem/Program.cs
+++ Teacher/HotelSystem/HotelSystem/Program.cs
@@ -34,18 +34,59 @@ app.UseRouting();
 app.UseAuthorization();
 
 
-//多層次路由
 
-//區域路由型態
+
+
+// 1. 先註冊特殊路由
+//多層次路由
 app.MapControllerRoute(
-    name: "default2",
+    name: "EmployeeRole",
+    pattern: "Manger/{action}/{id?}",
+    defaults: new { controller = "EmployeeRoles", action = "Index" });
+
+
+
+//自訂靜態路徑
+app.MapControllerRoute(
+    name: "about",
+    pattern: "about-us",
+    defaults: new { area = "Guest", controller = "Home", action = "Privacy" });
+
+
+
+// 帶有多個參數的路由
+app.MapControllerRoute(
+    name: "order",
+    pattern: "order/{orderId}/{productId}",
+    defaults: new { controller = "Order", action = "Detail" });
+
+
+app.MapControllerRoute(
+        name: "admin",
+        pattern: "Admin/{controller=Dashboard}/{action=Index}/{id?}");
+
+//2. 再註冊區域路由
+app.MapControllerRoute(
+    name: "areas",
+    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+
+
+// 3. 最後註冊預設路由
+
+app.MapControllerRoute(
+    name: "aaa",
+    pattern: "{area=User}/{controller=Home}/{action=Index}/{id?}");
+
+app.MapControllerRoute(
+    name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
 
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{area}/{controller}/{action}/{id?}");
+//app.MapControllerRoute(
+//    name: "default",
+//    pattern: "{area}/{controller}/{action}/{id?}");
 
 
 
