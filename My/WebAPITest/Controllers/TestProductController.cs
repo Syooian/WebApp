be8681d28d5731f82483147996b512b603707291
Syooian <msn0126@hotmail.com>
2025-08-04 11:23:13 +0800

    8.1. 實作Service及DI

---
+++ My/WebAPITest/Controllers/TestProductController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebAPITest.Services;
+
+namespace WebAPITest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestProductController : ControllerBase
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        TestProductService Service = new TestProductService();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public IEnumerable<TestProductData> GetProducts()
+        {
+            return Service.GetProducts();
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="ID"></param>
+        /// <returns></returns>
+        [HttpGet("{ID}")]
+        public ActionResult<TestProductData> GetProduct(int ID)
+        {
+            try
+            {
+                return Service.GetProduct(ID);
+            }
+            catch (Exception ex)
+            {
+                return NotFound();
+            }
+        }
+    }
+}
---
+++ My/WebAPITest/Services/TestProductService.cs
@@ -0,0 +1,40 @@
+﻿namespace WebAPITest.Services
+{
+    public struct TestProductData
+    {
+        public string ID;
+        public string Name;
+    }
+
+    public class TestProductService
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        TestProductData[] Products = new TestProductData[]
+        {
+            new TestProductData { ID = "A0001", Name = "Product A" },
+            new TestProductData { ID = "A0002", Name = "Product B" },
+            new TestProductData { ID = "A0003", Name = "Product C" }
+        };
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public TestProductData[] GetProducts()
+        {
+            return Products;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="ID"></param>
+        /// <returns></returns>
+        public TestProductData GetProduct(int ID)
+        {
+            return Products[ID];
+        }
+    }
+}
