620ca556516c077b1547e5a626690ede20a2f276
Syooian <msn0126@hotmail.com>
2025-08-04 13:51:20 +0800

    新增專案"HotelSystem"

---
+++ Teacher/HotelSystem/HotelSystem.Access/Data/HotelSysDBContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelSystem.Access.Data
+{
+    public class HotelSysDBContext : DbContext
+    {
+        public HotelSysDBContext(DbContextOptions<HotelSysDBContext> Options) : base(Options)
+        {
+
+        }
+    }
+}
