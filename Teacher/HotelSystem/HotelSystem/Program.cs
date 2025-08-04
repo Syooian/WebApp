edb25dbebb6820e821575c08e492053e50208461
Syooian <msn0126@hotmail.com>
2025-08-04 14:27:43 +0800

    路由調整、新增每個Area對應的Layout

--- Teacher/HotelSystem/HotelSystem/Program.cs
+++ Teacher/HotelSystem/HotelSystem/Program.cs
@@ -35,7 +35,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{area=Admin}/{controller=Home}/{action=Index}/{id?}");
 
 
 
