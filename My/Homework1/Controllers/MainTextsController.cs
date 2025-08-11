b4ce4a9fe3d1e85eac76c2fc76876a379607a6c1
Syooian <msn0126@hotmail.com>
2025-08-11 23:40:49 +0800

    將主文獨立成新的Controller

---
+++ My/Homework1/Controllers/MainTextsController.cs
@@ -0,0 +1,117 @@
+﻿using Homework1.Data;
+using Homework1.Models;
+using Homework1.Views.Shared;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Homework1.Controllers
+{
+    public class MainTextsController : Controller
+    {
+        readonly DBContext Context;
+        public MainTextsController(DBContext Context)
+        {
+            this.Context = Context;
+        }
+
+        /// <summary>
+        /// 主文清單
+        /// </summary>
+        /// <returns></returns>
+        public async Task<IActionResult> Index()
+        {
+            var Result = await Context.MainTexts.Include(R => R.Replies).OrderByDescending(T => T.CreatedDate).ToListAsync();
+
+            return View(Result);
+        }
+
+        /// <summary>
+        /// 新增主文
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="MainText"></param>
+        /// <param name="MainTextPhoto">圖片上傳</param>
+        /// <returns></returns>
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("MainTextID,Title,Content,UserName")] MainText MainText, IFormFile? MainTextPhoto)
+        {
+            if (ModelState.IsValid)
+            {
+                MainText.CreatedDate = DateTime.Now;
+
+                //圖片上傳
+                if (MainTextPhoto != null && MainTextPhoto.Length != 0)
+                {
+                    //只允許上傳圖片
+                    switch (MainTextPhoto.ContentType)
+                    {
+                        case "image/gif":
+                        case "image/bmp":
+                        case "image/jpg":
+                        case "image/jpeg":
+                        case "image/png":
+                        case "image/jfif":
+                            break;
+                        default:
+                            ViewData["PhotoError"] = "只允許上傳圖片";
+                            return View();
+                    }
+
+                    //取新檔名
+                    var FileName = MainText.MainTextID + Path.GetExtension(MainTextPhoto.FileName);
+
+                    var FilePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", Shared.MainTextPhotosPath, FileName);
+
+                    using (FileStream FS = new FileStream(FilePath, FileMode.Create))
+                    {
+                        MainTextPhoto.CopyTo(FS);
+                    }
+
+                    MainText.Photo = MainText.MainTextID;
+                    MainText.PhotoType = Path.GetExtension(MainTextPhoto.FileName);
+                }
+
+                Context.Add(MainText);
+                await Context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            //Console.WriteLine("ModelState is invalid. Errors: " + string.Join(", ", ModelState.Values.SelectMany(v => v.Errors).Select(e => e.ErrorMessage)));
+
+            return View(MainText);
+        }
+
+        /// <summary>
+        /// 詳細檢視
+        /// </summary>
+        /// <param name="MainTextID">主文編號</param>
+        /// <returns></returns>
+        public async Task<IActionResult> Details(string MainTextID)
+        {
+            if (string.IsNullOrEmpty(MainTextID))
+            {
+                return NotFound();
+            }
+
+            var MainText = await Context.MainTexts
+                .Include(M => M.Replies)
+                .FirstOrDefaultAsync(M => M.MainTextID == MainTextID);
+
+            if (MainText == null)
+            {
+                return NotFound();
+            }
+
+            return View(MainText);
+        }
+    }
+}
