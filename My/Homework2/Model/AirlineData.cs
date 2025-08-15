90ac107031aeca25371c61aaa8073f0beda31d05
Syooian <msn0126@hotmail.com>
2025-08-15 13:57:00 +0800

    新增模型

---
+++ My/Homework2/Model/AirlineData.cs
@@ -0,0 +1,39 @@
+﻿namespace Homework2.Model
+{
+    /// <summary>
+    /// 航空公司
+    /// </summary>
+    public class Airline
+    {
+        public string AirlineID { get; set; }
+        public AirlineName AirlineName { get; set; }
+        public AirlineNameAlias AirlineNameAlias { get; set; }
+        public string AirlineIATA { get; set; }
+        public string AirlineICAO { get; set; }
+        public string AirlineEmail { get; set; }
+        public string AirlineAddress { get; set; }
+        public string AirlinePhone { get; set; }
+        public string AirlineNationality { get; set; }
+        public DateTime UpdateTime { get; set; }
+        public int VersionID { get; set; }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class AirlineName
+    {
+        public string Zh_tw { get; set; }
+        public string En { get; set; }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class AirlineNameAlias
+    {
+        public string Zh_tw { get; set; }
+        public string En { get; set; }
+    }
+}
+
---
+++ My/Homework2/Model/AirportData.cs
@@ -0,0 +1,49 @@
+﻿namespace Homework2.Model
+{
+    /// <summary>
+    /// 機場
+    /// </summary>
+    public class Airport
+    {
+        public string AirportID { get; set; }
+        public AirportName AirportName { get; set; }
+        public string AirportIATA { get; set; }
+        public string AirportICAO { get; set; }
+        public AirportPosition AirportPosition { get; set; }
+        public AirportCityName AirportCityName { get; set; }
+        public string AirportAddress { get; set; }
+        public string AirportPhone { get; set; }
+        public string AirportNationality { get; set; }
+        public string AuthorityID { get; set; }
+        public DateTime UpdateTime { get; set; }
+        public int VersionID { get; set; }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class AirportName
+    {
+        public string Zh_tw { get; set; }
+        public string En { get; set; }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class AirportPosition
+    {
+        public int PositionLon { get; set; }
+        public int PositionLat { get; set; }
+        public string GeoHash { get; set; }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class AirportCityName
+    {
+        public string Zh_tw { get; set; }
+        public string En { get; set; }
+    }
+}
