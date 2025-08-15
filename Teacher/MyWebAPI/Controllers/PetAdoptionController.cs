f7c7341318850829145c5f713026e22b0f2097c7
Syooian <msn0126@hotmail.com>
2025-08-15 09:17:40 +0800

    9.2.1 將PetAdoptionController中的HttpClient物件寫成DI方式

--- Teacher/MyWebAPI/Controllers/PetAdoptionController.cs
+++ Teacher/MyWebAPI/Controllers/PetAdoptionController.cs
@@ -10,6 +10,13 @@ namespace MyWebAPI.Controllers
     [ApiController]
     public class PetAdoptionController : ControllerBase
     {
+        private readonly HttpClient _httpClient;
+
+        public PetAdoptionController()
+        {
+            _httpClient = new HttpClient();
+        }
+
         //9.1.5 撰寫Get()方法，使用HttpClient物件取得第三方API的資料
         [HttpGet]
         public async Task<IEnumerable<PetAdoptionData>> Get()
@@ -17,9 +24,9 @@ namespace MyWebAPI.Controllers
             string url = "https://data.moa.gov.tw/Service/OpenData/TransService.aspx?UnitId=QcbUEzN6E6DL&$top=200"; //這是資料來源
 
 
-            HttpClient client = new HttpClient();
+            //HttpClient client = new HttpClient();
 
-            var resp = await client.GetStringAsync(url);  //取得API的回應的Json字串
+            var resp = await _httpClient.GetStringAsync(url);  //取得API的回應的Json字串
 
             //將Json字串轉換為PetAdoptionData物件IEnumerable
             var collection = JsonConvert.DeserializeObject<IEnumerable<PetAdoptionData>>(resp);
