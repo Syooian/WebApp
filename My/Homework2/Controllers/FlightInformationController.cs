30123040fac7d92802adf7884f49749198daffa0
Syooian <msn0126@hotmail.com>
2025-08-15 14:13:03 +0800

    新增GetAirports, GetAirlines兩個Action；Controller調整繼承的基底，增加Route和ApiController標示

--- My/Homework2/Controllers/FlightInformationController.cs
+++ My/Homework2/Controllers/FlightInformationController.cs
@@ -1,9 +1,12 @@
-﻿using Homework2.Services;
+﻿using Homework2.Model;
+using Homework2.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Homework2.Controllers
 {
-    public class FlightInformationController : Controller
+    [Route("[controller]")]
+    [ApiController]
+    public class FlightInformationController : ControllerBase
     {
         /// <summary>
         /// 
@@ -18,9 +21,42 @@ namespace Homework2.Controllers
             this.Services = Services;
         }
 
-        public IActionResult Index()
+        /// <summary>
+        /// 取得所有機場資料
+        /// </summary>
+        /// <param name="PageSize"></param>
+        /// <param name="Page"></param>
+        /// <returns></returns>
+        [Route("GetAirports")]
+        [HttpGet]
+        public async Task<IEnumerable<Airport>> GetAirports(int PageSize = 30, int Page = 1)
         {
-            return View();
+            int Skip = (Page - 1) * PageSize;
+
+            var Result = await Services.GetAirports(Skip, PageSize);
+            if (Result == null)
+                return new List<Airport>();
+
+            return Result;
+        }
+
+        /// <summary>
+        /// 取得所有航空公司資料
+        /// </summary>
+        /// <param name="PageSize"></param>
+        /// <param name="Page"></param>
+        /// <returns></returns>
+        [Route("GetAirlines")]
+        [HttpGet]
+        public async Task<IEnumerable<Airline>> GetAirlines(int PageSize = 30, int Page = 1)
+        {
+            int Skip = (Page - 1) * PageSize;
+
+            var Result = await Services.GetAirlines(Skip, PageSize);
+            if (Result == null)
+                return new List<Airline>();
+
+            return Result;
         }
     }
 }
--- My/Homework2/Services/FlightInformationServices.cs
+++ My/Homework2/Services/FlightInformationServices.cs
@@ -1,4 +1,8 @@
-﻿namespace Homework2.Services
+﻿using Homework2.Model;
+using Newtonsoft;
+using Newtonsoft.Json;
+
+namespace Homework2.Services
 {
     public class FlightInformationServices
     {
@@ -13,6 +17,42 @@
         public FlightInformationServices(HttpClient Client)
         {
             this.Client = Client;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="Skip">跳過前幾筆</param>
+        /// <param name="Top">取前幾筆</param>
+        /// <returns></returns>
+        public async Task<IEnumerable<Airport>> GetAirports(int Skip = 0, int Top = 30)
+        {
+            var Resp = await Client.GetStringAsync($"https://tdx.transportdata.tw/api/basic/v2/Air/Airport?%24top={Top}&%24skip={Skip}&%24format=JSON");
+
+            //https://tdx.transportdata.tw/api/basic/v2/Air/Airport?%24top=30&%24format=JSON
+            //https://tdx.transportdata.tw/api/basic/v2/Air/Airport?%24top=30&%24skip=100&%24format=JSON
+
+            var Collection= JsonConvert.DeserializeObject<IEnumerable<Airport>>(Resp);
+
+            return Collection;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="Skip">跳過前幾筆</param>
+        /// <param name="Top">取前幾筆</param>
+        /// <returns></returns>
+        public async Task<IEnumerable<Airline>> GetAirlines(int Skip = 0, int Top = 30)
+        {
+            var Resp = await Client.GetStringAsync($"https://tdx.transportdata.tw/api/basic/v2/Air/Airline?%24top={Top}&%24skip={Skip}&%24format=JSON");
+
+            //https://tdx.transportdata.tw/api/basic/v2/Air/Airline?%24top=30&%24format=JSON
+            //https://tdx.transportdata.tw/api/basic/v2/Air/Airline?%24top=30&%24skip=0&%24format=JSON
+
+            var Collection = JsonConvert.DeserializeObject<IEnumerable<Airline>>(Resp);
+
+            return Collection;
         }
     }
 }
