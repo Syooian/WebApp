2b9eab1ad7e27fa893f8e3f83210a071b8b413ea
Syooian <msn0126@hotmail.com>
2025-08-15 13:03:15 +0800

    加入Controller, Services

---
+++ My/Homework2/Controllers/FlightInformationController.cs
@@ -0,0 +1,26 @@
+﻿using Homework2.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Homework2.Controllers
+{
+    public class FlightInformationController : Controller
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        readonly FlightInformationServices Services;
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="Services"></param>
+        public FlightInformationController(FlightInformationServices Services)
+        {
+            this.Services = Services;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ My/Homework2/Services/FlightInformationServices.cs
@@ -0,0 +1,18 @@
+﻿namespace Homework2.Services
+{
+    public class FlightInformationServices
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        HttpClient Client;
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="Client"></param>
+        public FlightInformationServices(HttpClient Client)
+        {
+            this.Client = Client;
+        }
+    }
+}
