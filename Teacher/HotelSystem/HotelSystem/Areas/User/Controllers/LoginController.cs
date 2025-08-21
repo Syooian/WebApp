38efc15c0f5e624a53bb5ad78f9a263d3ac75b40
Syooian <msn0126@hotmail.com>
2025-08-21 11:18:57 +0800

    密碼雜湊

--- Teacher/HotelSystem/HotelSystem/Areas/User/Controllers/LoginController.cs
+++ Teacher/HotelSystem/HotelSystem/Areas/User/Controllers/LoginController.cs
@@ -1,4 +1,6 @@
 ﻿using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
 using HotelSystem.Access.Data;
 using HotelSystem.Models;
 using Microsoft.AspNetCore.Authentication;
@@ -27,8 +29,15 @@ namespace HotelSystem.Areas.User.Controllers
         [HttpPost]
         public async Task<IActionResult> Login(MemberAccount memberAccount)
         {
+            //密碼必須先經過雜湊處理，再與資料庫中的密碼進行比對
+            if (memberAccount == null || string.IsNullOrEmpty(memberAccount.Account) || string.IsNullOrEmpty(memberAccount.Password))
+            {
+                ViewData["Error"] = "請輸入帳號和密碼";
+                return View();
+            }
 
-            var user = await _context.MemberAccount.FirstOrDefaultAsync(u => u.Account == memberAccount.Account && u.Password == memberAccount.Password);
+            var user = await _context.MemberAccount
+                .FirstOrDefaultAsync(u => u.Account == memberAccount.Account && u.Password == ComputeSha256Hash(memberAccount.Password)); //12345678
             
             
             if (user != null)
@@ -58,6 +67,24 @@ namespace HotelSystem.Areas.User.Controllers
 
 
          
+        }
+
+        // 新增一個靜態方法來進行 SHA256 雜湊
+        private static string ComputeSha256Hash(string rawData)
+        {
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                // 計算雜湊值
+                byte[] bytes = sha256Hash.ComputeHash(System.Text.Encoding.UTF8.GetBytes(rawData));
+
+                // 將 byte 陣列轉換成十六進位字串
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                return builder.ToString();
+            }
         }
     }
 }
